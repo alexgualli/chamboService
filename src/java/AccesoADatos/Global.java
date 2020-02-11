282e2a68d634fbfeb06a0828c31bf0d8cda87593
Usuario <Usuario@FI-LABC01-PC10>
2020-02-11 17:51:01 -0500

    First commit

---
+++ src/java/AccesoADatos/Global.java
@@ -0,0 +1,14 @@
+package AccesoADatos;
+
+public class Global {
+//    public static final String url="jdbc:postgresql://node78012-inventario.whelastic.net:5432/sistema_inventario";
+//    public static final String user="webadmin";
+//    public static final String pass="EFClgp23210";
+//    public static final String driver="org.postgresql.Driver";
+    
+    public static final  String url="jdbc:postgresql://localhost:5432/chambo";
+    public static final  String user="postgres";
+    public static final  String pass="admin";
+    public static final  String driver="org.postgresql.Driver";
+
+}
